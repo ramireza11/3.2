c29b5c6451431d0f0f9ad4d1dd730fb38a78be5a
ramireza11 <angel.g.ramirez@sjsu.edu>
2022-04-15 18:42:06 -0700

    hi

--- sketch.js
+++ sketch.js
@@ -1,7 +1,25 @@
+let state = 'title';
+
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
 }
 
 function draw() {
+
+  if(state ==='title') {
+    title();
+  } else if (state === 'level 1'){
+    level1();
+}
+
+function title(){
   background(220);
+  textSize(56);
+  stroke(255);
+  text('NINJA ACADEMY', 80, 80);
+}
+
+function level1(){
+  background(50, 150, 200);
 }
