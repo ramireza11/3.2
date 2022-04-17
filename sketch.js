d0513cc040a760496d709d383f6a8a6f8b80ffc4
ramireza11 <angel.g.ramirez@sjsu.edu>
2022-04-17 02:07:08 -0700

    update

--- sketch.js
+++ sketch.js
@@ -1,25 +1,77 @@
 let state = 'title';
-
+let cnv;
+let points = 0;
 
 function setup() {
-  createCanvas(600, 600);
+  cnv = createCanvas(600, 600);
+  
+  textFont('Courier New Bold');
 }
 
 function draw() {
 
-  if(state ==='title') {
-    title();
-  } else if (state === 'level 1'){
-    level1();
+  switch (state){
+    case 'title':
+      title();
+      cnv.mouseClicked(titleMouseClicked);
+      break;
+    case 'level 1':
+      level1();
+      cnv.mouseClicked(level1MouseClicked);
+      break;
+    case 'you win':
+      youWin();
+      cnv.mouseClicked(youWinMouseClicked);
+      break;
+    default:
+      break;
+  }
+
 }
 
 function title(){
-  background(220);
+  background(0);
   textSize(56);
-  stroke(255);
+  fill(255);
   text('NINJA ACADEMY', 80, 80);
+
+  textSize(30);
+  text('click here', 250, 500);
+}
+
+function titleMouseClicked(){
+  console.log('canvas is clicked on title page');
+  state = 'level 1'
 }
 
 function level1(){
   background(50, 150, 200);
+  text('click for points', 0, height - 30);
+
+}
+
+function level1MouseClicked(){
+  points++;
+  console.log('points = ' + points);
+  if (points >= 10){
+    state = 'you win'
+  }
+}
+
+function youWin(){
+  background(213,89,20);
+  textSize(56);
+  stroke(255);
+  textFont('Courier New Bold');
+  text('YOU WIN', 180, 80);
+
+  textSize(30);
+  textFont('Courier New Bold');
+  text('click anywhere to restart', 150, 550);
+}
+
+
+function youWinMouseClicked(){
+  state = 'level 1';
+  points = 0;
 }
